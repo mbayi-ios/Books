a057fa1209bbaacc381682531fd0e19c4748ddd0
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-05-19 13:08:26 +0300

    first commit

---
+++ Books/View Controllers/AddBookViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddBookViewController.swift
+//  Books
+//
+//  Created by Bart Jacobs on 21/01/2021.
+//
+
+import UIKit
+
+final class AddBookViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    @IBOutlet private var titleTextField: UITextField!
+    @IBOutlet private var authorTextField: UITextField!
+
+    // MARK: - Actions
+    
+    @IBAction func addBook(_ sender: Any) {
+        
+    }
+
+}
---
+++ Books/View Controllers/BooksViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Books
+//
+//  Created by Bart Jacobs on 21/01/2021.
+//
+
+import UIKit
+
+final class BooksViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    @IBOutlet private var booksLabel: UILabel!
+    
+}
