95e31e725b3f4a13a108b90fabf7e4f80f8ba3bc
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-05-19 13:31:20 +0300

    creating the persistent container

--- Books/View Controllers/BooksViewController.swift
+++ Books/View Controllers/BooksViewController.swift
@@ -1,16 +1,32 @@
-//
-//  ViewController.swift
-//  Books
-//
-//  Created by Bart Jacobs on 21/01/2021.
-//
-
 import UIKit
+import CoreData
 
 final class BooksViewController: UIViewController {
 
     // MARK: - Properties
     
     @IBOutlet private var booksLabel: UILabel!
+
+    private lazy var persistentContainer: NSPersistentContainer = {
+        NSPersistentContainer(name: "Books")
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        persistentContainer.loadPersistentStores { [weak self] persistentStoreDescription, error  in
+            if let error = error {
+                print("unable to add persistent store")
+                print("\(error), \(error.localizedDescription)")
+            } else {
+                self?.fetchBooks()
+                //print(persistentStoreDescription.url, persistentStoreDescription.type)
+            }
+        }
+    }
+
+    private func fetchBooks() {
+        print(persistentContainer.viewContext)
+    }
     
 }
